249856f949eca4a3816667d83d10c1c2e6ced0df
Hariston Nunes Macedo <85318716+HaristonNuMacedo@users.noreply.github.com>
2024-09-25 14:51:35 -0300

    teste

---
+++ js/functionAbrirPaginacao.js
@@ -0,0 +1,121 @@
+function abrir01() {
+    var visaoG = document.querySelector('#visaoGeral')
+    visaoG.style.background = "#00b2f0";
+    visaoG.style.color = "black";
+
+    var visaoG02 = document.querySelector('#visaoGeral02')
+    visaoG02.style.background = "#222";
+    visaoG02.style.color = "white";
+
+    var visaoG03 = document.querySelector('#visaoGeral03')
+    visaoG03.style.background = "#222";
+    visaoG03.style.color = "white";
+
+    var visaoG04 = document.querySelector('#visaoGeral04')
+    visaoG04.style.background = "#222";
+    visaoG04.style.color = "white";
+
+    var abrirJanela04 = document.querySelector('#testeInserir04')
+    abrirJanela04.style.display = "none";
+
+    var abrirJanela03 = document.querySelector('#testeInserir03')
+    abrirJanela03.style.display = "none";
+
+    var abrirJanela02 = document.querySelector('#testeInserir02')
+    abrirJanela02.style.display = "none";
+
+    var abrirJanela = document.querySelector('#testeInserir')
+    abrirJanela.style.display = "block";
+}
+
+function abrir02() {
+    var visaoG02 = document.querySelector('#visaoGeral02')
+    visaoG02.style.background = "#00b2f0";
+    visaoG02.style.color = "#000";
+
+    var visaoG = document.querySelector('#visaoGeral')
+    visaoG.style.background = "#222";
+    visaoG.style.color = "white";
+
+    var visaoG03 = document.querySelector('#visaoGeral03')
+    visaoG03.style.background = "#222";
+    visaoG03.style.color = "white";
+
+    var visaoG04 = document.querySelector('#visaoGeral04')
+    visaoG04.style.background = "#222";
+    visaoG04.style.color = "white";
+
+    var abrirJanela04 = document.querySelector('#testeInserir04')
+    abrirJanela04.style.display = "none";
+
+    var abrirJanela03 = document.querySelector('#testeInserir03')
+    abrirJanela03.style.display = "none";
+
+    var abrirJanela = document.querySelector('#testeInserir')
+    abrirJanela.style.display = "none";
+
+    var abrirJanela02 = document.querySelector('#testeInserir02')
+    abrirJanela02.style.display = "block";
+}
+
+function abrir03() {
+    var visaoG03 = document.querySelector('#visaoGeral03')
+    visaoG03.style.background = "#00b2f0";
+    visaoG03.style.color = "#000";
+
+    var visaoG = document.querySelector('#visaoGeral')
+    visaoG.style.background = "#222";
+    visaoG.style.color = "white";
+
+    var visaoG02 = document.querySelector('#visaoGeral02')
+    visaoG02.style.background = "#222";
+    visaoG02.style.color = "white";
+
+    var visaoG04 = document.querySelector('#visaoGeral04')
+    visaoG04.style.background = "#222";
+    visaoG04.style.color = "white";
+
+    var abrirJanela04 = document.querySelector('#testeInserir04')
+    abrirJanela04.style.display = "none";
+
+    var abrirJanela02 = document.querySelector('#testeInserir02')
+    abrirJanela02.style.display = "none";
+
+    var abrirJanela = document.querySelector('#testeInserir')
+    abrirJanela.style.display = "none";
+
+    var abrirJanela03 = document.querySelector('#testeInserir03')
+    abrirJanela03.style.display = "block";
+}
+
+
+function abrir04() {
+    var visaoG04 = document.querySelector('#visaoGeral04')
+    visaoG04.style.background = "#00b2f0";
+    visaoG04.style.color = "#000";
+
+    var visaoG = document.querySelector('#visaoGeral')
+    visaoG.style.background = "#222";
+    visaoG.style.color = "white";
+
+    var visaoG02 = document.querySelector('#visaoGeral02')
+    visaoG02.style.background = "#222";
+    visaoG02.style.color = "white";
+
+    var visaoG03 = document.querySelector('#visaoGeral03')
+    visaoG03.style.background = "#222";
+    visaoG03.style.color = "white";
+
+    var abrirJanela03 = document.querySelector('#testeInserir03')
+    abrirJanela03.style.display = "none";
+
+    var abrirJanela02 = document.querySelector('#testeInserir02')
+    abrirJanela02.style.display = "none";
+
+    var abrirJanela = document.querySelector('#testeInserir')
+    abrirJanela.style.display = "none";
+
+    var abrirJanela04 = document.querySelector('#testeInserir04')
+    abrirJanela04.style.display = "block";
+}
+
